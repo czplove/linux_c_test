74382949600b7a649abda6d82a015f5338d44597
taidalab <zdajun1988@163.com>
2017-09-28 02:18:26 -0700

    add sqlite3 test

---
+++ sqlite3/sqlite3_t.c
@@ -0,0 +1,51 @@
+//-gcc -o sqlite_t sqlite3_t.c -lsqlite3
+#include <stdio.h>
+#include <stdlib.h>
+  
+#include <sqlite3.h>
+  
+int myfunc(void *p, int argc, char **argv, char **argvv)
+{
+          int i;
+          *(int *)p = 0;
+          for(i =0; i < argc; i++)
+          {
+                  printf("%s = %s ", argvv[i], argv[i] ? argv[i] :"NULL");
+          }
+          putchar('\n');
+          return 0;
+}
+  
+int main(int argc, char *argv[])
+{
+          sqlite3 *db;
+          char *err =0;
+          int ret =0;
+          int empty =1;
+  
+          ret = sqlite3_open("student",&db);
+          if(ret !=SQLITE_OK)
+          {
+                  fputs("/n",stderr);
+                  exit(1);
+          }
+          ret = sqlite3_exec(db, "select * from user;", myfunc, &empty, &err);
+  
+          if(ret != SQLITE_OK)
+          {
+                  fputs(err,stderr);
+                  fputs("/n",stderr);
+                  sqlite3_close(db);
+                  exit(1);
+          }
+  
+          if(empty)
+          {
+                  fputs("table student is empty/n", stderr);
+                  exit(1);
+          }
+  
+          sqlite3_close(db);
+  
+          return 0;
+}
